8057793dcaa4c6bc028813e2921a6c87bd65cc59
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2023-06-02 21:29:33 +0900

    임시 commit

--- app/src/main/java/com/gcu/dongdong2/club/ClubTemp.java
+++ app/src/main/java/com/gcu/dongdong2/club/ClubTemp.java
@@ -1,3 +1,4 @@
+
 package com.gcu.dongdong2.club;
 
 import android.content.Intent;
@@ -6,62 +7,99 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.gcu.dongdong2.FindFragment;
+import com.gcu.dongdong2.databinding.ClubTempBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.gcu.dongdong2.MainActivity;
 import com.gcu.dongdong2.R;
 
 public class ClubTemp extends Fragment {
 
+    private ClubTempBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.club_temp, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.club_temp, container, false);
 
-        Button Back_btn = rootView.findViewById(R.id.btnApplyTemp);
-        Back_btn.setOnClickListener(new View.OnClickListener() {
+        if (binding == null) {
+            binding = ClubTempBinding.inflate(inflater, container, false);
+        }
+
+        binding.btnApplyTemp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getContext(), ClubApplyActivity.class);
-                startActivity(intent);
+                // 여기다 이벤트 작성
+
+                ((MainActivity)getActivity()).replaceFragment(new ClubApplyActivity());
+//                startActivity(intent);
+
             }
         });
 
-        Button Back_btn2 = rootView.findViewById(R.id.btnExploreTemp);
-        Back_btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getContext(), ClubExploreActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button Back_btn3 = rootView.findViewById(R.id.btnPostTemp);
-        Back_btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getContext(), ClubPostActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button Back_btn4 = rootView.findViewById(R.id.btnPostSearchedTemp);
-        Back_btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getContext(), ClubSearchedActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button Back_btn5 = rootView.findViewById(R.id.btnPostSearchingTemp);
-        Back_btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getContext(), ClubSearchingActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        return rootView;
+        return binding.getRoot();
     }
+
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.club_temp);
+//
+//
+//        Button Back_btn = (Button) findViewById(R.id.btnApplyTemp);
+//        Back_btn.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(getApplicationContext(), ClubApplyActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button Back_btn2 = (Button) findViewById(R.id.btnExploreTemp);
+//        Back_btn2.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(getApplicationContext(), ClubExploreActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//
+//        Button Back_btn3 = (Button) findViewById(R.id.btnPostTemp);
+//        Back_btn3.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(getApplicationContext(), ClubPostActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button Back_btn4 = (Button) findViewById(R.id.btnPostSearchedTemp);
+//        Back_btn4.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(getApplicationContext(), ClubSearchedActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button Back_btn5 = (Button) findViewById(R.id.btnPostSearchingTemp);
+//        Back_btn5.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(getApplicationContext(), ClubSearchingActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 }
