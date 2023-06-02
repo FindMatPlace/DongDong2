7d7855b658331c2786c90725664753fc3ad5103d
Hyunstone <hyun104467@naver.com>
2023-06-02 11:41:16 +0900

    feat: Setting Frag에서 다른 Frag 연결

---
+++ app/src/main/java/com/gcu/dongdong2/MainActivity.java
@@ -0,0 +1,66 @@
+package com.gcu.dongdong2;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private HomeFragment fragment_home;
+    private FindFragment fragment_find;
+    private AlarmFragment fragment_alarm;
+    private SettingFragment fragment_setting;
+    private FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        fragment_home = new HomeFragment();
+        fragment_find = new FindFragment();
+        fragment_alarm = new AlarmFragment();
+        fragment_setting = new SettingFragment();
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment_home).commitNowAllowingStateLoss();
+
+        // 이벤트 리스너 등록
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new MenuClickEventListener());
+    }
+    class MenuClickEventListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.tab_home) {
+                fragmentTransaction.replace(R.id.frameLayout, fragment_home).commitNowAllowingStateLoss();
+            } else if (itemId == R.id.tab_find) {
+                fragmentTransaction.replace(R.id.frameLayout, fragment_find).commitNowAllowingStateLoss();
+            } else if (itemId == R.id.tab_alarm) {
+                fragmentTransaction.replace(R.id.frameLayout, fragment_alarm).commitNowAllowingStateLoss();
+            } else if (itemId == R.id.tab_setting) {
+                fragmentTransaction.replace(R.id.frameLayout, fragment_setting).commitNowAllowingStateLoss();
+            }
+
+            return true;
+        }
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment).commit();      // Fragment로 사용할 MainActivity내의 layout공간을 선택합니다.
+    }
+}
---
+++ app/src/main/java/com/gcu/dongdong2/SettingFragment.java
@@ -0,0 +1,96 @@
+package com.gcu.dongdong2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.gcu.dongdong2.databinding.FragmentIdpasswordBinding;
+import com.gcu.dongdong2.databinding.FragmentUserSettingBinding;
+import com.gcu.dongdong2.userInfo.ChangePasswordFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private FragmentUserSettingBinding binding;
+    private FragmentUserSettingBinding getBinding() {
+        return binding;
+    }
+
+    private FragmentTransaction fragmentTransaction;
+    private FragmentManager fragmentManager;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SettingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SettingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SettingFragment newInstance(String param1, String param2) {
+        SettingFragment fragment = new SettingFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (binding == null) {
+            binding = FragmentUserSettingBinding.inflate(inflater, container, false);
+        }
+        binding.btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(requireContext(), "이미지 버튼 눌림", Toast.LENGTH_SHORT);
+                ((MainActivity)getActivity()).replaceFragment(new ChangePasswordFragment());
+            }
+        });
+        
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+}
