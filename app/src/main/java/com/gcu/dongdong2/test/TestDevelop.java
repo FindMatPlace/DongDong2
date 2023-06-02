13ef906d7415ba2c353937205d71678d458c5b38
Hyunstone <hyun104467@naver.com>
2023-06-02 17:39:30 +0900

    feat: 테스트 환경 준비2

--- app/src/main/java/com/gcu/dongdong2/test/TestDevelop.java
+++ app/src/main/java/com/gcu/dongdong2/test/TestDevelop.java
@@ -24,7 +24,6 @@ public class TestDevelop extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     private void groupCreate(GroupCreateDto groupInfo) {
